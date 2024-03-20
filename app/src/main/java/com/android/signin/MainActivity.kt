617a60bafe17b060d249d87252f309bd45402301
cococo35 <taejun0513@gmail.com>
2024-03-20 16:56:18 +0900

    Lv2/SignIn Branch 생성 & UI 구현

--- app/src/main/java/com/android/signin/MainActivity.kt
+++ app/src/main/java/com/android/signin/MainActivity.kt
@@ -8,31 +8,31 @@ import android.widget.EditText
 import android.widget.Toast
 
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val idText = findViewById<EditText>(R.id.idread)
-        val passWordText = findViewById<EditText>(R.id.passwordread)
-        val signInButton = findViewById<Button>(R.id.signinbutton)
-        val registerButton = findViewById<Button>(R.id.registerbutton)
+        val id_text = findViewById<EditText>(R.id.id_read)
+        val password_text = findViewById<EditText>(R.id.password_read)
+        val signin_button = findViewById<Button>(R.id.signin_button)
+        val register_button = findViewById<Button>(R.id.register_button)
 
-        signInButton.setOnClickListener {
-            if(idText.text.isEmpty()) {
+        signin_button.setOnClickListener {
+            if(id_text.text.isEmpty()) {
                 Toast.makeText(this, "아이디를 확인해주세요",Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
 
-            if(passWordText.text.isEmpty()) {
+            if(password_text.text.isEmpty()) {
                 Toast.makeText(this, "비밀번호를 확인해주세요",Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
 
             Toast.makeText(this, "로그인 성공",Toast.LENGTH_LONG).show()
 
-            val id:String = idText.text.toString()
-            val passWord:String = passWordText.text.toString()
+            val id:String = id_text.text.toString()
+            val passWord:String = password_text.text.toString()
 
             val intent = Intent(this, HomeActivity::class.java)
             intent.putExtra("id", id)
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        registerButton.setOnClickListener {
+        register_button.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
